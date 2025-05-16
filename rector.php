acab958ea6285de2b6fe00e6801c4196da7277e7
Morten Scheel <morten@mortenscheel.com>
2025-05-16 17:24:24 +0200

    Initial commit

---
+++ rector.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+use Rector\Config\RectorConfig;
+
+return RectorConfig::configure()
+    ->withPaths([
+        __DIR__.'/app',
+        __DIR__.'/tests',
+        __DIR__.'/config',
+        __DIR__.'/database',
+    ])
+    ->withPreparedSets(
+        deadCode: true,
+        codeQuality: true,
+        typeDeclarations: true,
+        privatization: true,
+        earlyReturn: true,
+        strictBooleans: true,
+    )
+    ->withPhpSets();
